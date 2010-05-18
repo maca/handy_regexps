be4c917387444d17579facdda7777dbcf6022f3a
Macario <macarui@gmail.com>
2010-05-18 13:23:31 -0500

    Initial commit to hady_regexps.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'hady_regexps'
+
+class Test::Unit::TestCase
+end
